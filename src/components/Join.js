a69132b4d2b58bf25166689b9a5243c57d647330
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-13 00:56:39 +0600

    animation edited

--- src/components/Join.js
+++ src/components/Join.js
@@ -2,20 +2,23 @@ import React from 'react';
 import image from '../Images/Glasses.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBaby } from '@fortawesome/free-solid-svg-icons'
+import { Roll } from 'react-reveal';
 
 
 const Join = () => {
     return (
         <div style={{ backgroundColor: "#5A5B5C" }} className="pb-32">
-            <div className="flex border border-dashed border-2 w-1/2 mx-auto justify-around py-32">
-                <div>
-                    <p className="text-center text-6xl">JOIN OUR <br /> DISCORD</p>
-                    <a className="flex justify-center text-3xl mt-4 border py-2 px-2 mr-12" href="#">DISCORD &nbsp;  <FontAwesomeIcon icon={faBaby} /></a>
+            <Roll left>
+                <div className="flex border border-dashed border-2 w-1/2 mx-auto justify-around py-32">
+                    <div>
+                        <p className="text-center text-6xl">JOIN OUR <br /> DISCORD</p>
+                        <a className="flex justify-center text-3xl mt-4 border py-2 px-2 mr-12" href="#">DISCORD &nbsp;  <FontAwesomeIcon icon={faBaby} /></a>
+                    </div>
+                    <div style={{ backgroundImage: `url(${""})`, backgroundPosition: 'top left', backgroundRepeat: 'no-repeat' }}>
+
+                    </div>
                 </div>
-                <div style={{ backgroundImage: `url(${""})`, backgroundPosition: 'top left', backgroundRepeat: 'no-repeat'}}>
-                    
-                </div>
-            </div>
+            </Roll>
         </div>
     );
 };
--- src/components/OurVision.js
+++ src/components/OurVision.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Fade } from 'react-reveal';
 import glass from '../Images/Glasses.png'
 import verticleLine from '../Images/Vertical-Lines.png'
 
 const OurVision = () => {
     return (
         <div style={{ backgroundColor: "#5A5B5C" }} className=" pt-36 pb-32">
-            <div className="w-3/5 mx-auto p-12" style={{ backgroundImage: `url(${verticleLine})`, backgroundPosition: 'top left', backgroundRepeat: 'no-repeat', backgroundSize: '2% 100%' }}>
-                <div style={{ backgroundImage: `url(${glass})`, backgroundSize: 'cover', backgroundPosition: 'center right', backgroundSize: '55% 110%', backgroundRepeat: 'no-repeat' }} className="h-100">
-                    <p className="text-5xl w-1/2">Our Vision</p>
-                    <p className="text-xl w-96 mt-3.5">Internet made is both a brand
-                        and movement bringing artisits,
-                        creators and fashion desingers
-                        together to use technology to
-                        express themeselves in a new way</p>
+            <Fade>
+                <div className="w-3/5 mx-auto p-12" style={{ backgroundImage: `url(${verticleLine})`, backgroundPosition: 'top left', backgroundRepeat: 'no-repeat', backgroundSize: '2% 100%' }}>
+                    <div style={{ backgroundImage: `url(${glass})`, backgroundSize: 'cover', backgroundPosition: 'center right', backgroundSize: '55% 110%', backgroundRepeat: 'no-repeat' }} className="h-100">
+                        <p className="text-5xl w-1/2">Our Vision</p>
+                        <p className="text-xl w-96 mt-3.5">Internet made is both a brand
+                            and movement bringing artisits,
+                            creators and fashion desingers
+                            together to use technology to
+                            express themeselves in a new way</p>
+                    </div>
                 </div>
-            </div>
+            </Fade>
         </div>
     );
 };
