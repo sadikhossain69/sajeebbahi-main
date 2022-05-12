f8511ae5488ccaa3f1f779471e7f28d3eacef6b4
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-12 21:01:41 +0600

    Roadmap done

--- src/components/OurVision.js
+++ src/components/OurVision.js
@@ -4,7 +4,7 @@ import verticleLine from '../Images/Vertical-Lines.png'
 
 const OurVision = () => {
     return (
-        <div style={{ backgroundColor: "#5A5B5C" }} className="pt-36 pb-32">
+        <div style={{ backgroundColor: "#5A5B5C" }} className=" pt-36 pb-32">
             <div className="w-3/5 mx-auto p-12" style={{ backgroundImage: `url(${verticleLine})`, backgroundPosition: 'top left', backgroundRepeat: 'no-repeat', backgroundSize: '2% 100%' }}>
                 <div style={{ backgroundImage: `url(${glass})`, backgroundSize: 'cover', backgroundPosition: 'center right', backgroundSize: '55% 110%', backgroundRepeat: 'no-repeat' }} className="h-100">
                     <p className="text-5xl w-1/2">Our Vision</p>
--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -3,23 +3,49 @@ import { BiLockAlt } from 'react-icons/bi'
 
 const Roadmap = () => {
     return (
-        <div className=' h-screen py-14' style={{ backgroundColor: "#5A5B5C" }}>
+        <div className='py-14' style={{ backgroundColor: "#5A5B5C" }}>
             <h2 className=' text-center uppercase text-9xl'>Roadmap</h2>
-            <div className=' flex justify-center space-x-5'>
-                <div className=' hover:bg-slate-600 hover:duration-300 border w-96 h-64 p-5'>
-                    <p className=' text-slate-400 text-2xl'>Q2</p>
-                    <p className=' text-4xl'>PHYGITAL</p>
-                    <div className=' mt-28 flex justify-start space-x-3 items-center'>
-                        <p><BiLockAlt/></p>
-                        <p className=' text-xl'>LOCKED CONTENT</p>
+            <div className=''>
+                <div className=' flex justify-center space-x-5'>
+                    <div className=' hover:bg-slate-600 hover:duration-300 border w-96 h-64 p-5'>
+                        <p className=' text-slate-400 text-2xl'>Q2</p>
+                        <p className='font-bold text-4xl'>PHYGITAL</p>
+                        <div className=' mt-28 flex justify-start space-x-3 items-center'>
+                            <p><BiLockAlt /></p>
+                            <p className=' text-xl'>LOCKED CONTENT</p>
+                        </div>
+                    </div>
+                    <div className=' hover:bg-slate-600 hover:duration-300 border w-96 h-64 p-5'>
+                        <p className=' text-slate-400 text-2xl'>Q2</p>
+                        <p className='font-bold text-4xl'>DEVELOPING ECOSYSTEM</p>
+                        <div className=' mt-16 flex justify-start space-x-3 items-center'>
+                            <p><BiLockAlt /></p>
+                            <p className=' text-xl'>LOCKED CONTENT</p>
+                        </div>
                     </div>
                 </div>
-                <div className=' hover:bg-slate-600 hover:duration-300 border w-96 h-64 p-5'>
-                    <p className=' text-slate-400 text-2xl'>Q2</p>
-                    <p className=' text-4xl'>DEVELOPING ECOSYSTEM</p>
-                    <div className=' mt-16 flex justify-start space-x-3 items-center'>
-                        <p><BiLockAlt/></p>
-                        <p className=' text-xl'>LOCKED CONTENT</p>
+                <div style={{ width: '58.5%', }} className=' border mt-3 mx-auto h-64'>
+                    <div className=' p-5'>
+                        <p className=' text-slate-400 text-2xl'>Q3</p>
+                        <p className=' font-bold text-4xl'>COMMUNITY</p>
+                    </div>
+                </div>
+                <div className=' mt-3 flex justify-center space-x-5'>
+                    <div className=' hover:bg-slate-600 hover:duration-300 border w-96 h-64 p-5'>
+                        <p className=' text-slate-400 text-2xl'>Q2</p>
+                        <p className='font-bold text-4xl'>PHYGITAL</p>
+                        <div className=' mt-28 flex justify-start space-x-3 items-center'>
+                            <p><BiLockAlt /></p>
+                            <p className=' text-xl'>LOCKED CONTENT</p>
+                        </div>
+                    </div>
+                    <div className=' hover:bg-slate-600 hover:duration-300 border w-96 h-64 p-5'>
+                        <p className=' text-slate-400 text-2xl'>Q2</p>
+                        <p className='font-bold text-4xl'>DEVELOPING ECOSYSTEM</p>
+                        <div className=' mt-16 flex justify-start space-x-3 items-center'>
+                            <p><BiLockAlt /></p>
+                            <p className=' text-xl'>LOCKED CONTENT</p>
+                        </div>
                     </div>
                 </div>
             </div>
